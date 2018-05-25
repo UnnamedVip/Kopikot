c2aad9c1eef0f214ab432c960ee34553764e3bb5
Unnamed <31389643+UnnamedVip@users.noreply.github.com>
2018-05-25 13:49:30 +0500

    Good

--- background.js
+++ background.js
@@ -1,47 +1,56 @@
-//function for icon change in extension if user opened url = https://www.kopikot.ru/home
+//#region function for icon change in extension if user opened url = https://www.kopikot.ru/home
 var check = function(){
-  
   chrome.tabs.getAllInWindow(undefined, function(tabs) {
-  for (var i = 0;i<tabs.length; i++) {
-
-    if (tabs[i].url && (tabs[i].url.indexOf("https://www.kopikot.ru/home")!=-1)) {
-
-      chrome.browserAction.setIcon({
-        path : "assets/images/active.png"
-      });
-
-      return;
+    for (var i = 0;i<tabs.length; i++) {
+      if (tabs[i].url && (tabs[i].url.indexOf("https://www.kopikot.ru/home")!=-1)) {
+        chrome.browserAction.setIcon({
+          path : "assets/images/active.png"
+        });
+        return;
+      }
     }
-  }
-    chrome.browserAction.setIcon({
-    path : "assets/images/default.png"
+      chrome.browserAction.setIcon({
+      path : "assets/images/default.png"
+    });
   });
-  
-});
-
 };
 
 chrome.tabs.onUpdated.addListener(check);
 
 chrome.tabs.onRemoved.addListener(check);
+//#endregion
 
+//#region count click:
+var count;
 
-/* chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-    alert(response.farewell);
-  });
-}); */
+chrome.storage.local.get(['key'], function(result) {
+  if(result.key == undefined){
+    count = 0;
+    chrome.storage.local.set({key: count}, function() {});
+  }
+  else{
+    count =  result.key;
+  }
+});
+
 chrome.runtime.onConnect.addListener(function(port) {
   
   console.assert(port.name == "portForCountClicks");
-  
+
   port.onMessage.addListener(function(msg) {
-    if(msg.clicksCount != undefined){
-      alert(msg.clicksCount);
-      port.postMessage({answer: "ok"});
-    }
     
+    count = count + msg.clicksCount;
+    
+    if(count < 10){
+      chrome.storage.local.set({key: count}, function() {
+      });
+    }
+    else{
+      alert("10 clicks. You can not click more!");
+      count = 0; 
+      chrome.storage.local.set({key: count}, function() {});
+    }
+      
   });
-  
-  
 });
+//#endregion
