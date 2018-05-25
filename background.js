7d8c3318e2acc656113a4308ddbd9f32e895bf1c
Unnamed <31389643+UnnamedVip@users.noreply.github.com>
2018-05-25 11:44:07 +0500

    pipe port

--- background.js
+++ background.js
@@ -19,10 +19,29 @@ var check = function(){
   
 });
 
-
 };
 
 chrome.tabs.onUpdated.addListener(check);
 
 chrome.tabs.onRemoved.addListener(check);
 
+
+/* chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+    alert(response.farewell);
+  });
+}); */
+chrome.runtime.onConnect.addListener(function(port) {
+  
+  console.assert(port.name == "portForCountClicks");
+  
+  port.onMessage.addListener(function(msg) {
+    if(msg.clicksCount != undefined){
+      alert(msg.clicksCount);
+      port.postMessage({answer: "ok"});
+    }
+    
+  });
+  
+  
+});
--- clickCount.js
+++ clickCount.js
@@ -1,11 +1,41 @@
 var count = 0;
+var port = chrome.runtime.connect({name: "portForCountClicks"});
 
 document.body.onclick = function(){
     
     count++;
-    
+
     if (count == 10){
         console.log("Stop clicks");
+
+        
+        port.postMessage({clicksCount: count});
+
+        port.onMessage.addListener(function(msg) {
+            if (msg.answer == "ok"){
+                console.log("answer get");
+            }
+              
+        });
+        
+        count = 0;
     }
     
 };
+
+
+
+/* chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+    console.log(request.greeting);
+     sendResponse({farewell: "goodbye"});
+}); */
+
+var port = chrome.runtime.connect({name: "knockknock"});
+port.postMessage({joke: "Knock knock"});
+port.onMessage.addListener(function(msg) {
+  if (msg.question == "Who's there?")
+    port.postMessage({answer: "Madame"});
+  else if (msg.question == "Madame who?")
+    port.postMessage({answer: "Madame... Bovary"});
+});
